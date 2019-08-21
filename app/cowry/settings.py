723850249d853f77de336a365f8d02c585eb97b8
t4christ <bakaretemitayo7@gmail.com>
2019-08-21 19:52:01 +0100

    modify dockerrun file

--- app/cowry/settings.py
+++ app/cowry/settings.py
@@ -140,7 +140,7 @@ DATABASES = {
         'NAME': os.environ.get('POSTGRES_DATABASE','postgres'),
         'USER': os.environ.get('POSTGRES_USER','postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD','postgres'),
-        'HOST': os.environ.get('POSTGRES_HOST','db'),
+        'HOST': os.environ.get('POSTGRES_HOST','postgres'),
         'PORT': os.environ.get('POSTGRES_PORT','5432'),
     }
 }
